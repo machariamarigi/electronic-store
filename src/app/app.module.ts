a31a57037cb906e618381f8771d5913a1524099d
machariamarigi <machariamarigi@gmail.com>
2019-12-02 19:08:54 +0300

    GET method for all products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {
+  MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule,
+  MatCardModule, MatSelectModule, MatDialogModule, MatBadgeModule, MatSnackBarModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import {
-  MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule,
-  MatCardModule, MatSelectModule, MatDialogModule, MatBadgeModule, MatSnackBarModule } from '@angular/material';
+
 
 const matDesignModules = [
   MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule,
@@ -22,7 +24,8 @@ const matDesignModules = [
     BrowserModule,
     BrowserAnimationsModule,
     LayoutModule,
-   matDesignModules
+    matDesignModules,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
